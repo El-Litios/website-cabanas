d03947006e7d927146427f89b594d4a995c331dd
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-13 23:37:42 -0300

    ajuste slider

--- src/components/principal/Slider.vue
+++ src/components/principal/Slider.vue
@@ -4,11 +4,11 @@
             <ul class="slides">
                 <li>
                     <div class="parallax">
-                        <img src="../../assets/WALLPAPER.jpg">
+                        <img src="../../assets/WALLPAPER-radal.jpg" class="img-responsive">
                     </div>
                     <div class="caption center-align">
-                    <h3>This is our big Tagline!</h3>
-                    <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
+                        <h3>This is our big Tagline!</h3>
+                        <h5 class="light grey-text text-lighten-3">Here's our small slogan.</h5>
                     </div>
                 </li>
             </ul>
@@ -22,13 +22,20 @@ export default {
             document.addEventListener('DOMContentLoaded', function() {
             var elems = document.querySelectorAll('.slider');
             var instances = M.Slider.init(elems, {
-                height: 700
+                height: 450
             });
         });
     }
 }
 </script>
 
-<style>
-
+<style scoped>
+    .slider .slides li .caption{
+        background-color: red;
+        border-radius: 10px;
+        top: 25%;
+        left: 40%;
+        width: 20%;
+        height: 45%;
+    }
 </style>

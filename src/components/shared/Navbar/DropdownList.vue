655b378809b807dc7a79e068625a1658acfb0f93
El-Litios <carlosguzmanrojas00@gmail.com>
2022-01-16 19:53:25 -0300

    Ultimos ajustes

--- src/components/shared/Navbar/DropdownList.vue
+++ src/components/shared/Navbar/DropdownList.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
         <ul id="dropdown1" class="dropdown-content">
-            <li><router-link :to="{ name:'cabin-id', params: {id:5} }">Cabaña 1</router-link></li>
-            <li><a href="#!">Cabaña 2</a></li>
-            <li><a href="#!">Cabaña 3</a></li>
-            <li><a href="#!">Cabaña 4</a></li>
-            <li><a href="#!">Cabaña 5</a></li>
-            <li><a href="#!">Cabaña 6</a></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:1} }">Cabaña 1</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:2} }">Cabaña 2</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:3} }">Cabaña 3</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:4} }">Cabaña 4</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:5} }">Cabaña 5</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:6} }">Cabaña 6</router-link></li>
         </ul>
 
         <ul id="dropdown2" class="dropdown-content">
-            <li><a href="#!">Cabaña 1</a></li>
-            <li><a href="#!">Cabaña 2</a></li>
-            <li><a href="#!">Cabaña 3</a></li>
-            <li><a href="#!">Cabaña 4</a></li>
-            <li><a href="#!">Cabaña 5</a></li>
-            <li><a href="#!">Cabaña 6</a></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:1} }">Cabaña 1</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:2} }">Cabaña 2</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:3} }">Cabaña 3</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:4} }">Cabaña 4</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:5} }">Cabaña 5</router-link></li>
+            <li><router-link :to="{ name:'cabin-id', params: {id:6} }">Cabaña 6</router-link></li>
         </ul>
   </div>
 </template>
